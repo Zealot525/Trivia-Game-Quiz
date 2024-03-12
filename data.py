e2f3fc535679c6a7cc90648d01d275158850b336
Jonathan Chin <132028541+Zealot525@users.noreply.github.com>
2024-03-12 21:06:53 +0900

    Add files via upload

---
+++ data.py
@@ -0,0 +1,18 @@
+# for more trivia questions, go to https://opentdb.com/
+
+question_data = [{
+    "type":"boolean",
+    "difficulty":"medium",
+    "category":"General Knowledge",
+    "question":"The US emergency hotline is 911 because of the September 11th terrorist attacks.",
+    "correct_answer":"False",
+    "incorrect_answers":["True"],
+    },
+    {"type":"boolean",
+     "difficulty":"medium",
+     "category":"General Knowledge",
+     "question":"Fast food restaurant chains Carl's Jr. and Hardee's are owned by the same company.",
+     "correct_answer":"True",
+     "incorrect_answers":["False"]
+     },
+     {"type":"boolean","difficulty":"medium","category":"General Knowledge","question":"Albert Einstein had trouble with mathematics when he was in school.","correct_answer":"False","incorrect_answers":["True"]},{"type":"boolean","difficulty":"medium","category":"General Knowledge","question":"&quot;Buffalo buffalo Buffalo buffalo buffalo buffalo Buffalo buffalo.&quot; is a grammatically correct sentence.","correct_answer":"True","incorrect_answers":["False"]},{"type":"boolean","difficulty":"medium","category":"General Knowledge","question":"Kissing someone for one minute burns about 2 calories.","correct_answer":"True","incorrect_answers":["False"]},{"type":"boolean","difficulty":"medium","category":"General Knowledge","question":"The pickled gherkin was first added to hamburgers because a US health law required all fast-food to include a source of Vitamin C.","correct_answer":"False","incorrect_answers":["True"]},{"type":"boolean","difficulty":"medium","category":"General Knowledge","question":"The French word for &quot;glass&quot; is &quot;glace&quot;.","correct_answer":"False","incorrect_answers":["True"]},{"type":"boolean","difficulty":"medium","category":"General Knowledge","question":"Furby was released in 1998.","correct_answer":"True","incorrect_answers":["False"]},{"type":"boolean","difficulty":"medium","category":"General Knowledge","question":"&quot;Santa Claus&quot; is a variety of melon.","correct_answer":"True","incorrect_answers":["False"]},{"type":"boolean","difficulty":"medium","category":"General Knowledge","question":"Instant mashed potatoes were invented by Canadian Edward Asselbergs in 1962.","correct_answer":"True","incorrect_answers":["False"]},{"type":"boolean","difficulty":"medium","category":"General Knowledge","question":"Sitting for more than three hours a day can cut two years off a person&#039;s life expectancy.","correct_answer":"True","incorrect_answers":["False"]},{"type":"boolean","difficulty":"medium","category":"General Knowledge","question":"Haggis is traditionally ate on Burns Night.","correct_answer":"True","incorrect_answers":["False"]},{"type":"boolean","difficulty":"medium","category":"General Knowledge","question":"An eggplant is a vegetable.","correct_answer":"False","incorrect_answers":["True"]},{"type":"boolean","difficulty":"medium","category":"General Knowledge","question":"You are allowed to sell your soul on eBay.","correct_answer":"False","incorrect_answers":["True"]},{"type":"boolean","difficulty":"medium","category":"General Knowledge","question":"Cucumbers are usually more than 90% water.","correct_answer":"True","incorrect_answers":["False"]}]
